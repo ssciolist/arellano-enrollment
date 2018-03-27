d8f318b4f8fef80e8dbb4e761bd544551326ed92
Megan Arellano <ssciolist@gmail.com>
2018-03-27 13:47:59 -0600

    refactors test a bit. Pry isn't installed

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,8 +4,21 @@ class StudentsController < ApplicationController
     render :index
   end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    require 'pry'; binding.pry
+  end
+
   def show
     @student = Student.find(params[:id])
     render :show
   end
+
+  private
+
+    def student_params
+    end
 end
--- spec/features/students/user_can_create_a_new_student_spec.rb
+++ spec/features/students/user_can_create_a_new_student_spec.rb
@@ -2,17 +2,21 @@ require 'rails_helper'
 
 describe 'As a user' do
   describe 'when I visit /students/new' do
-    describe 'I can submit a new student form' do
-      scenario 'and I can submit it, then see a student\'s page' do
+    scenario 'I can fill in a new student form' do
+      visit '/students/new'
+
+      fill_in 'student[name]', with: 'Riley'
+    end
+
+    scenario 'and I can submit it, then see a student\'s page' do
         visit '/students/new'
 
-        fill_in 'student[name]', with: 'Riley'
+        fill_in 'student[name]', with: 'Diana'
         click_button 'Create Student'
 
         student = Student.all.last
 
         expect(current_path).to eq(student_path(student))
-      end
     end
   end
 end
