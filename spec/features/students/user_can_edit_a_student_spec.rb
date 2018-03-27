d4c5c49d515af901e6375c1d6a65d53436686ca9
Megan Arellano <ssciolist@gmail.com>
2018-03-27 14:00:44 -0600

    failing student edit test

---
+++ spec/features/students/user_can_edit_a_student_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'As a user' do
+  describe 'when I visit /students/:id/edit' do
+    scenario 'I can fill in a form' do
+      visit edit_student_path
+
+      fill_in 'student[name]', with: 'Barb'
+    end
+
+    scenario 'I can submit the form' do
+      student = Student.create!(name: 'Megan')
+      visit edit_student_path(student)
+
+      fill_in 'student[name]', with: 'Laura'
+      click_button 'Edit Student'
+
+      expect(current_path).to eq(student_path(student))
+      expect(page).to have_content('Laura')
+    end
+  end
+end
