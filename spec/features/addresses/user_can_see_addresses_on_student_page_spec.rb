e222a531c0f279438e481ba80f97686a33ddd4d8
Megan Arellano <ssciolist@gmail.com>
2018-03-27 15:46:00 -0600

    failing test for student addresses on page

---
+++ spec/features/addresses/user_can_see_addresses_on_student_page_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'As a user' do
+  describe 'when I visit /students/:id' do
+    scenario 'I see that student\'s addresses' do
+      student = Student.create!(name: 'Jacob')
+      address = student.addresses.create!(description: 'Home',
+                                street: '1234 Fake st',
+                                city: 'Boulder',
+                                state: 'CO',
+                                zip_code: 34569)
+      address_2 = student.addresses.create!(description: 'Work',
+                                street: '567 Fake st',
+                                city: 'Denver',
+                                state: 'UT',
+                                zip_code: 59854)
+      visit student_path(student)
+
+      expect(page).to have_content(address)
+      expect(page).to have_content(address_2)
+    end
+  end
+end
