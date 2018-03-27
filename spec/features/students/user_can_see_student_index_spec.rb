fd6f6de6550e21ba7b19d44c7d0979b0d2593f18
Megan Arellano <ssciolist@gmail.com>
2018-03-27 14:52:35 -0600

    adds links to student names

--- spec/features/students/user_can_see_student_index_spec.rb
+++ spec/features/students/user_can_see_student_index_spec.rb
@@ -13,5 +13,20 @@ describe 'As a user' do
     expect(page).to have_content(student_2.name)
     expect(page).to have_content(student_3.name)
     end
+
+    scenario 'and each student\'s name is a link' do
+      student = Student.create!(name: 'Jacob')
+      student_2 = Student.create!(name: 'Seth')
+      student_3 = Student.create!(name: 'Travis')
+
+      visit students_path
+      click_link 'Jacob'
+
+      visit students_path
+      click_link 'Seth'
+
+      visit students_path
+      click_link 'Travis'
+    end
   end
 end
